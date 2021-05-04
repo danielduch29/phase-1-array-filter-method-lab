966a0664ebea8440513da3dcfbd7112f93adecb2
Daniel <Daniel.duchatellier@gmail.com>
2021-05-03 20:50:55 -0400

    commit tree c635bfedf38ae1c06c4483d89e4993a6bec1fd6e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+
+function findMatching(arr, name){
+    const newArr = []
+    for(const search of arr){
+        
+        if(search.toUpperCase() === name.toUpperCase()){
+            newArr.push(search)
+        }
+    }
+    return newArr
+}
+
+function fuzzyMatch(arr, str){
+    const newArr = [];
+    for(const search of arr){
+       if(search.startsWith(str)){
+            newArr.push(search)
+       }
+    }
+    return newArr;
+}
+
+function matchName(obj, str){
+    const newArr = [];
+    for(const search of obj){
+        if(search.name === str){
+            newArr.push(search)
+        }
+    }
+    return newArr;
+}
